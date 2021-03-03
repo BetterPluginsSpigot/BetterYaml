2b3a38a693f9c3e7475a7c5d84e9289d2af6b205
Dieter Nuytemans <Nuytemans_Dieter@hotmail.com>
2021-03-03 11:16:13 +0100

    Improve documentation + bump version

--- src/main/java/be/dezijwegel/betteryaml/YamlMerger.java
+++ src/main/java/be/dezijwegel/betteryaml/YamlMerger.java
@@ -7,6 +7,11 @@ public class YamlMerger {
 
     private final Map<String, Object> defaultOptions;
 
+    /**
+     * Prepare default options to be merged with live options
+     *
+     * @param defaultOptions the default key-value pairs, to be used when no others are available
+     */
     public YamlMerger(Map<String, Object> defaultOptions)
     {
         this.defaultOptions = defaultOptions;
@@ -16,6 +21,8 @@ public class YamlMerger {
      * Merge the given options with the stored defaults
      * The provided options will get priority over default values
      * Any missing values in the provided map will be filled in by the default values
+     * All keys in the provided map, that are not in the defaultOptions (provided through constructor), will be ignored
+     *
      * @param options the options to be merged with all default values
      * @return the merged Map
      */
--- src/main/java/be/dezijwegel/betteryaml/files/YamlReader.java
+++ src/main/java/be/dezijwegel/betteryaml/files/YamlReader.java
@@ -14,11 +14,27 @@ public class YamlReader {
 
     private final Map<String, Object> contents = new HashMap<>();
 
+    /**
+     * Creates a YamlReader from a given File
+     * YamlReader supports nested keys
+     * All key-value pairs will be added to a Map
+     *
+     * @param file the file to be read
+     * @throws IOException when a stream cannot be created or the file does not exist
+     */
     public YamlReader(File file) throws IOException
     {
         this( new FileInputStream( file ) );
     }
 
+    /**
+     * Creates a YamlReader from a given InputStream
+     * YamlReader supports nested keys
+     * All key-value pairs will be added to a Map
+     *
+     * @param fis the InputStream to be read
+     * @throws IOException when a stream cannot be created or the file does not exist
+     */
     public YamlReader(InputStream fis) throws IOException
     {
         LoaderOptions loaderOptions = new LoaderOptions();
@@ -34,11 +50,26 @@ public class YamlReader {
         fis.close();
     }
 
+    /**
+     * Get the contents from the provided File or InputStream
+     * ConfigurationSections are not added separately
+     * Nested keys are saved as: section.subsection.option
+     *
+     * @return all keys and their mapped values from within the file
+     */
     public Map<String, Object> getContents()
     {
         return this.contents;
     }
 
+    /**
+     * Recursively read a config option
+     * Calls itself when a option has subsections
+     * Adds a key-value pair to the contents Map if the path does not have subsections
+     *
+     * @param entry the key-value pair under consideration
+     * @param path the previous path, to keep track of nested keys
+     */
     private void addRecursiveContents(Map.Entry<String, Object> entry, String path)
     {
         String key = entry.getKey();
