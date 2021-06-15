7a04bb73b4f9c306954a2fce158f49f780486749
ProfliX <lacimus321>
2021-06-15 21:19:37 +0200

    Removed java docs

--- javaDocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"be.dezijwegel.betteryaml"},{"l":"be.dezijwegel.betteryaml.files"},{"l":"be.dezijwegel.betteryaml.interfaces"},{"l":"be.dezijwegel.betteryaml.util"}];updateSearchResults();
